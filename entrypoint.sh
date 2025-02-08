036f918278bc18ce496c6da5aeece76e1d0e6312
Alex Fu <alexfu@fastmail.com>
2025-02-08 12:04:38 -0500

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+set -e
+
+RELEASE_ID=$1
+ACCESS_TOKEN=$2
+APP_FILE=$3
+
+curl --fail-with-body "https://api.appdeploypro.com/releases/$RELEASE_ID" \
+  -X PUT \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -F binary="@$APP_FILE"
